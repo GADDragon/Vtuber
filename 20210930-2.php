079047e4a6f08cc78e5b78979fbc6bb24775ff57
Vtuber <sky362478sky22569@gmail.com>
2022-01-26 21:31:10 +0800

    1/26上傳

---
+++ 20210930-2.php
@@ -0,0 +1,9 @@
+<?php
+      echo '[
+{"id" : "001" ,"img" : "Gawr_Gura.png" ,"height":"141cm","birthday":"6/20","design":"甘城なつき","herf":"bs-20211222-holoEN-About-Gura.html","title" : "Gawr_Gura"},
+{"id" : "002" ,"img" : "ina3.jpg" ,"height":"157cm","birthday":"5/20","design":"黑星紅白","title" : "Ninomae Ina nis"},
+{"id" : "003" ,"img" : "unnamed.jpg" ,"height":"150cm","birthday":"1/6","design":"なび","title" : "Watson Amelia"},
+{"id" : "004" ,"img" : "cali.jpg" ,"height":"167cm","birthday":"4/4","design":"ゆきさめ","title" : "Mori Calliope"},
+{"id" : "005" ,"img" : "kiara.jpg" ,"height":"165cm","birthday":"7/6","design":"huke","title" : "Takanashi Kiara"}
+]'
+?>
